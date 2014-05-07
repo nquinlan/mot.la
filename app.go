97e07edb611f0d7eeca6b45d3f7d998c9236351c
scottmotte <scott@scottmotte.com>
2014-05-07 13:39:00 -0700

    Change content

--- app.go
+++ app.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"github.com/codegangsta/martini-contrib/render"
 	"github.com/go-martini/martini"
 )
 
 func main() {
 	m := martini.Classic()
-	m.Use(render.Renderer())
 
 	m.Get("/", func() string {
-		return "mot.la - a home of <a href='http://scottmotte.com'>scottmotte</a>"
+		return "<html><body><p>mot.la</p><ul><li>a home of <a href='http://scottmotte.com'>scottmotte</a></li><li>I'm on <a href='http://twitter.com/scottmotte'>twitter</a></ul><p><img src='https://avatars2.githubusercontent.com/u/3848?s=460'></p></body></html>"
 	})
 
 	m.Run()
